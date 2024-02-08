dc427ffbd91e9bafa127d45e260f2456bc503f95
rugbel <ruggero.bellio@uniud.it>
2024-02-08 19:26:56 +0100

    documentation update

--- R/arcProbit.R
+++ R/arcProbit.R
@@ -1,18 +1,15 @@
 #' arcProbit: All-row-col fitting of categorical data models with crossed random effects
 #'
 #'
-#' The \code{arcProbit} package can be used to estimate the parameters of a p2 model
-#' for directed binary networks with correlated random effects. It implements
-#' (approximate) maximum likelihood estimation, following the methodology
-#' studied in ARC REF.
-#'
-#' A vignette is available with some illustrative examples.
+#' The \code{arcProbit} package can be used to estimate the parameters of mixed models for binary
+#' and ordinal data with crossed random effects. It implements the ARC estimation proposed in
+#' https://arxiv.org/abs/2308.15681. A vignette is available with some illustrative examples.
 #'
 #'
 #' @docType package
 #' @name arcProbit
 #' @references Bellio, R., Ghosh, S., Owen, A. and Varin, C. (2023). Scalable Estimation of Probit Models with
-#' Crossed Random Effects. Manuscript in preparation.
+#' Crossed Random Effects. https://arxiv.org/abs/2308.15681
 #' @useDynLib binCross
 #' @useDynLib ordCross
 #' @useDynLib arcProbit, .registration = TRUE
